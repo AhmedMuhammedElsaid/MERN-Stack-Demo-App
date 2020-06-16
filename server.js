1070417e4f632a1040d4e0fbfd550b38dc58d635
AhmedMuhammedElsaid <ahmed.muhammed.elsaid@gmail.com>
2020-06-16 03:25:07 +0200

    Edit Server

--- server.js
+++ server.js
@@ -34,9 +34,7 @@ app.get('/slotMachine',(req,res)=>{
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
+ 
 }
 
 
