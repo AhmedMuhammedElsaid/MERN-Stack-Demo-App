61068abfd892afd495db82200195ace116b7e6de
AhmedMuhammedElsaid <ahmed.muhammed.elsaid@gmail.com>
2020-06-16 17:07:49 +0200

    second Commit

--- server.js
+++ server.js
@@ -10,31 +10,33 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false })), app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 // First Function :) 
-app.get(`/`,  async(req, res) => {
+app.get(`/`, async (req, res) => {
   console.log(req.body)
   let data = await Functions.getCountryByName();
   await res.json(data);
 });
 
 // Second Function
-app.get('/countries/',async (req,res)=>{
+app.get('/countries/', async (req, res) => {
   let strArr = ['dubai', 'ger', 'land', 'den', 'france', 'egy', 'Belgium']
   let data = await Functions.getCommonCountries(strArr);
-  await res.json({data:data,strArr:strArr})
+  await res.json({ data: data, strArr: strArr })
 })
 
 //Third Function
-app.get('/slotMachine',(req,res)=>{
-  let data= Functions.slotMachine(20)
+app.get('/slotMachine', (req, res) => {
+  let data = Functions.slotMachine(20)
   res.json(data)
 })
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
- 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.htm"));
+  })
 }
 
 
